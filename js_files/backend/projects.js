5aad8a5049ce22e38cefebdc87155cf45d93e937
sudhucodes <sudhanshukumar72508@gmail.com>
2024-09-23 21:27:33 +0530

    fixed bug

--- js_files/backend/projects.js
+++ js_files/backend/projects.js
@@ -4,7 +4,7 @@ const projects = [
   { name: "Login-SignUp-page", codeUrl: "codes/sourcecode.html", hasAssets: false, category: "htmlcss" },
   { name: "Calculator-UI-design", codeUrl: "codes/sourcecode.html", hasAssets: false, category: "htmlcss" },
   { name: "YouTube-clone-full-Responsive", codeUrl: "codes/sourcecode.html", hasAssets: true, category: "htmlcss" },
-  { name: "Social-media-links-button", codeUrl: "codes/sourcecode.html", hasAssets: false, category: "ghtmlcss" },
+  { name: "Social-media-links-button", codeUrl: "codes/sourcecode.html", hasAssets: false, category: "htmlcss" },
   { name: "Responsive-sidebar-menu", codeUrl: "codes/sourcecode.html", hasAssets: true, category: "htmlcss" },
   { name: "Landing-page1", codeUrl: "codes/sourcecode.html", hasAssets: true, category: "htmlcss" },
   { name: "Login-page4", codeUrl: "codes/sourcecode.html", hasAssets: true, category: "htmlcss" },
