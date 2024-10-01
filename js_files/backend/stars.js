d377ea13ad8105193d93f781805ace8996a8c1fa
sudhucodes <sudhanshukumar72508@gmail.com>
2024-10-01 20:32:43 +0530

    Added 2 stars in SudhuCodes stars

--- js_files/backend/stars.js
+++ js_files/backend/stars.js
@@ -1,20 +1,24 @@
 // Array of star data
 const stars = [
-    { imgSrc: 'images/stars-image/no-user-image.jpg', name: 'Rajesh Kumar', project: 'Spotify Clone Project', profileLink: 'https://www.instagram.com/', rating: 4 },
-    { imgSrc: 'images/stars-image/no-user-image.jpg', name: 'Mohit Singh', project: 'YouTube Clone Project', profileLink: 'https://www.instagram.com/', rating: 3 },
+    { imgSrc: 'images/stars-image/rajesh-kumar.jpg', name: 'Rajesh Kumar', project: 'Spotify Clone Project', profileLink: '', rating: 4 },
+    { imgSrc: 'images/stars-image/mohit-singh.jpg', name: 'Mohit Singh', project: 'YouTube Clone Project', profileLink: '', rating: 3 },
     { imgSrc: 'images/stars-image/sudhanshu-kumar.png', name: 'Sudhanshu Kumar', project: 'E-Commerce Website', profileLink: 'https://www.instagram.com/ig__sudhanshu__/', rating: 3 },
-    { imgSrc: 'images/stars-image/no-user-image.jpg', name: 'Aman Kumar', project: 'Portfolio Website', profileLink: 'https://www.instagram.com/_its.aman04/', rating: 4 }
+    { imgSrc: 'images/stars-image/aman-kumar.jpg', name: 'Aman Kumar', project: 'Portfolio Website', profileLink: 'https://www.instagram.com/_its.aman04/', rating: 4 }
 ];
+
 function renderStars(filteredStars) {
     const container = document.querySelector('.stars-container');
     const starsHtml = filteredStars.map(star => {
         const ratingStars = Array(star.rating).fill('<img src="images/icons/star-emoji-icon.svg" alt="Star">').join('');
+        const profileHtml = star.profileLink ? 
+            `<a target="_blank" href="${star.profileLink}">Profile <i class="fa-solid fa-arrow-up-right-from-square"></i></a>` :
+            '<span>No Profile</span>';
 
         return `
             <div class="star">
                 <div class="star-profile">
                     <div class="star-profile-link">
-                    <a target="_blank" href="${star.profileLink}">Profile <i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+                        ${profileHtml}
                     </div>
                     <img class="star-image" src="${star.imgSrc}" alt="${star.name}">
                     <div class="star-rating">
