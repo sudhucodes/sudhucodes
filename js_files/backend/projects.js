fb763330f54377664e1b6ae807ef0f5583547f20
sudhucodes <sudhanshukumar72508@gmail.com>
2024-10-03 10:42:22 +0530

    Added new project in TailwindCSS, Project name ~ Premium Subscription Pricing Card with Tailwind CSS and Feature List

--- js_files/backend/projects.js
+++ js_files/backend/projects.js
@@ -1,5 +1,6 @@
 // Project List
 const projects = [
+  {"name": "Premium Subscription Pricing Card with Tailwind CSS and Feature List", "shortName": "Pricing Card - TailwindCSS", "projectId": "TAILWINDCSS-PC-T-20241003-PD8WO", "codeUrl": "codes/sourcecode.html", "hasAssets": false, "category": "tailwindCSS"},
   {"name": "A Professional and User-Friendly Review Section Design Using TailwindCSS", "shortName": "Review section - TailwindCSS", "projectId": "TAILWINDCSS-RS-T-20241002-FCKLI", "codeUrl": "codes/sourcecode.html", "hasAssets": true, "category": "tailwindCSS"},
   {"name": "Hostinger Domain Plan Pricing Table Clone - Using TailwindCSS", "shortName": "Pricing Table - TailwindCSS", "projectId": "TAILWINDCSS-PT-T-20241002-KV8UN", "codeUrl": "codes/sourcecode.html", "hasAssets": false, "category": "tailwindCSS"},
   {"name": "Advanced Payment Form with TailwindCSS & Smooth User Interface", "shortName": "TailwindCSS Payment Interface Form", "projectId": "TAILWINDCSS-TPIF-20241001-RUWO5", "codeUrl": "codes/sourcecode.html", "hasAssets": true, "category": "tailwindCSS"},
